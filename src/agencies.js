8323372a6e1838a028e9ff55fdebaa6bc7b75926
markmisener <markmisener@gmail.com>
2021-05-15 16:27:04 -0700

    initial commit

---
+++ src/agencies.js
@@ -0,0 +1,35 @@
+const URL = 'http://webservices.nextbus.com/service/publicXMLFeed?command=agencyList'
+
+fetch(URL)
+  .then(response => response.text())
+  .then(str => (new window.DOMParser()).parseFromString(str, "text/xml"))
+  .then(xmlDoc => xmlDoc.getElementsByTagName("agency"))
+  .then(xmlFeatures => {
+    let features = [];
+    for (i = 0; i < xmlFeatures.length; i++) {
+      let feature = xmlFeatures[i];
+      if (feature.getAttribute("regionTitle") === "California-Northern") {
+        let parsedFeature = {
+          "tag": feature.getAttribute("tag"),
+          "title": feature.getAttribute("title")
+        }
+        features.push(parsedFeature)
+      }
+    }
+    return features
+  })
+  .then(parsedFeatures => {
+    let agencySelect = document.getElementById('agency');
+
+    let existingOptions = [];
+    for (i = 0; i < agencySelect.options.length; i++){
+      existingOptions.push(agencySelect.options[i].value);
+    }
+
+    for (i = 0; i < parsedFeatures.length; i++) {
+      let feature = parsedFeatures[i];
+      if (!existingOptions.includes(feature.tag)) {
+        agencySelect.options.add(new Option(feature.title, feature.tag));
+      }
+    }
+  })
---
+++ src/map.js
@@ -0,0 +1,112 @@
+const XMLFEED = 'http://webservices.nextbus.com/service/publicXMLFeed';
+const SOURCE_NAME = 'nextbus';
+const LAYER_NAME = 'bus-locations';
+const INTERVAL_SECONDS = 10;
+const INTERVAL_MILLISECONDS = INTERVAL_SECONDS * 1000;
+
+mapboxgl.accessToken = 'pk.eyJ1IjoibW1pc2VuZXIiLCJhIjoiY2tpbnNodXAyMTYzaDJ5cnh2MzhhZTdrOSJ9.2kqiWW_ezo14wsaONE-MjQ';
+
+var map = new mapboxgl.Map({
+  container: 'map',
+  style: 'mapbox://styles/mapbox/streets-v11',
+  center: [-122.4390, 37.7666],
+  zoom: 11
+});
+
+map.on('load', () => {
+  // add empty source
+  map.addSource(SOURCE_NAME, {
+    type: 'geojson',
+    data: {
+      type: 'FeatureCollection',
+      features: []
+    }
+  });
+  map.addLayer({
+    id: LAYER_NAME,
+    source: SOURCE_NAME,
+    type: 'circle',
+    paint: {
+      'circle-radius': 6,
+      'circle-color': '#B42222'
+    }
+  });
+
+  map.on('mouseenter', LAYER_NAME, function() {
+    map.getCanvas().style.cursor = 'pointer';
+  });
+
+  map.on('mouseleave', LAYER_NAME, function() {
+    map.getCanvas().style.cursor = '';
+  });
+
+  map.on('click', LAYER_NAME, function(e) {
+    if (Object.keys(e.features[0].properties).length > 0) {
+      var htmlStr = '<div><ul>';
+      Object.entries(e.features[0].properties).forEach(entry => {
+        htmlStr = htmlStr.concat(`<li>${entry[0]}: ${entry[1]}</li>`)
+      });
+      htmlStr.concat('</ul></div>')
+
+      new mapboxgl.Popup()
+        .setLngLat(e.lngLat)
+        .setHTML(htmlStr)
+        .addTo(map);
+    }
+  });
+
+  // update now
+  updateSource();
+
+  // update every intervalSeconds
+  setInterval(function() {
+    updateSource();
+  }, INTERVAL_MILLISECONDS);
+});
+
+function updateSource() {
+  let agency = document.querySelector('#agency').value;
+  let url = `${XMLFEED}?command=vehicleLocations&a=${agency}&t=0`
+  fetch(url)
+    .then(response => response.text())
+    .then(str => (new window.DOMParser()).parseFromString(str, "text/xml"))
+    .then(xmlDoc => xmlDoc.getElementsByTagName("vehicle"))
+    .then(xmlFeatures => {
+      let features = [];
+      for (i = 0; i < xmlFeatures.length; i++) {
+        let feature = xmlFeatures[i];
+        let parsedFeature = {
+          "type": "Feature",
+          "properties": {
+            "id": feature.getAttribute("id"),
+            "routeTag": feature.getAttribute("routeTag"),
+            "secsSinceReport": feature.getAttribute("secsSinceReport"),
+            "heading": feature.getAttribute("heading"),
+            "speedKmHr": feature.getAttribute("speedKmHr")
+          },
+          "geometry": {
+            "type": "Point",
+            "coordinates": [
+              parseFloat(feature.getAttribute("lon")),
+              parseFloat(feature.getAttribute("lat"))
+            ]
+          }
+        };
+        features.push(parsedFeature);
+      };
+      return features
+    })
+    .then(features => {
+      if (features.length > 0) {
+        let featureCollection = {
+          type: 'FeatureCollection',
+          features: features
+        }
+        map.getSource(SOURCE_NAME).setData(featureCollection);
+        let bounds = turf.bbox(featureCollection);
+        map.fitBounds(bounds, {padding: 20});
+      } else {
+        alert(`No active vehicles found for ${agency}.`)
+      }
+    });
+}
