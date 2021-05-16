2d707931e3a0633fa1fda78df69f952c2f3e7021
markmisener <markmisener@gmail.com>
2021-05-15 19:57:24 -0700

    use proxy service

--- src/agencies.js
+++ src/agencies.js
@@ -1,6 +1,4 @@
-const REDIRECT_SERVICE = 'https://traffic-cop.herokuapp.com/http-redirect';
-const XMLFEED = 'http://webservices.nextbus.com/service/publicXMLFeed?command=agencyList'
-const URL = `${REDIRECT_SERVICE}?url=${XMLFEED}`
+const URL = `${REDIRECT_SERVICE}?url=http://webservices.nextbus.com/service/publicXMLFeed?command=agencyList`
 
 fetch(URL)
   .then(response => response.text())
