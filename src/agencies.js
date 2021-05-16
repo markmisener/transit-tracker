e60dfa41c04873280e190447fc0b53a77c910d0d
markmisener <markmisener@gmail.com>
2021-05-15 20:47:21 -0700

    don't redefine redirect service

--- src/agencies.js
+++ src/agencies.js
@@ -1,7 +1,4 @@
-const REDIRECT_SERVICE = 'https://traffic-cop.herokuapp.com/https-redirect';
-const URL = `${REDIRECT_SERVICE}?url=http://webservices.nextbus.com/service/publicXMLFeed?command=agencyList`
-
-fetch(URL, {
+fetch("https://traffic-cop.herokuapp.com/https-redirect?url=http://webservices.nextbus.com/service/publicXMLFeed?command=agencyList", {
     mode: "no-cors"
   })
   .then(response => response.text())
